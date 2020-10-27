c314600d2f7b6a6b6b0f143c0e6609ee2e90d2fe
leticia-sena <everton4732@gmail.com>
2020-10-26 22:09:10 -0300

    Exercício bando de dados relacional

---
+++ MySQL/lista_01/Exercicio1.sql
@@ -0,0 +1,49 @@
+
+
+insert into colaboradores (nome,data_nascimento,sexo,cargo,salario)values (
+"Cassia Rosário", 
+"1988-04-04", 
+"F", 
+"Psicóloga", 
+3500.00 );
+
+insert into colaboradores (nome,data_nascimento,sexo,cargo,salario)values (
+"Alexandre Souza", 
+"1985-04-04", 
+"M", 
+"Contador", 
+3800.00 );
+
+insert into colaboradores (nome,data_nascimento,sexo,cargo,salario)values (
+"Lucas Oliveira", 
+"1993-08-04", 
+"A", 
+"Estagiário", 
+1500.00 );
+
+insert into colaboradores (nome,data_nascimento,sexo,cargo,salario)values (
+"Leticia Sena", 
+"1998-10-14", 
+"F", 
+"Desenvolvedora", 
+4500.00 );
+
+insert into colaboradores (nome,data_nascimento,sexo,cargo,salario)values (
+"Everton Luiz", 
+"1989-09-29", 
+"M", 
+"Analista de Suporte", 
+3000.00 );
+
+insert into colaboradores (nome,data_nascimento,sexo,cargo,salario)values (
+"Luan Laurentino", 
+"2002-04-05", 
+"M", 
+"Analista", 
+1900.00 );
+
+select *  from colaboradores
+where salario > 2000.00;
+
+select *  from colaboradores
+where salario < 2000.00
